c9d857b8f6cd7bb45eed0067f836ef15b154c660
yuriHyun <eziezir55@gmail.com>
2024-06-08 01:10:42 +0900

    팀매칭 성공함+팀상태 업데이트+전공 레이아웃에서 텍스트만 바꿈

--- app/src/main/java/com/example/teamplay_p/front/UserList.java
+++ app/src/main/java/com/example/teamplay_p/front/UserList.java
@@ -12,6 +12,7 @@ public class UserList {
 
 
 
+
     public UserList(int profile_img, String username, String meetingrole, String usernumber, String major1, String major2,String major3){
         this.profile_img = profile_img;
         Username = username;
@@ -20,6 +21,7 @@ public class UserList {
         Major1 = major1;
         Major2 = major2;
         Major3 = major3;
+
 
     }
 
